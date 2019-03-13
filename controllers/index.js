cdf156d3b927b2d1dff9d9222b686c7d9b634fde
yuting6304 <s5333815@yahoo.com.tw>
2019-03-13 22:55:19 +0800

    Successfully using database

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
 var xoauth2 = require('xoauth2');
+var mysql  = require('mysql');  
 var router = express.Router();
 
 /* GET home page. */
@@ -8,32 +9,62 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 router.post('/login', function(req, res, next){
-    var account = req.body.account;
-    var password = req.body.password;
+    let account = req.body.account;
+    let password = req.body.password;
     console.log("Account : " + account + ", Password : " + password);
 });
 
 router.post('/register1', function(req, res, next){
-    var name = req.body.name;
-    var account = req.body.account;
-    var password = req.body.password;
-    console.log("Name : " + name + ", Account : " + account + ", Password : " + password);
+    let name = req.body.name;
+    let account = req.body.account;
+    let password = req.body.password;
+
+    var connection = mysql.createConnection({     
+        host     : 'localhost',       
+        user     : 'db',              
+        password : 'qweszxc6304',       
+        port: '3306',                   
+        database: 'bc_project' 
+      }); 
+       
+      connection.connect();
+       
+      var  addSql = 'INSERT INTO users(name, account, password) VALUES(?,?,?)';
+      var  addSqlParams = [name, account,password];
+      //增
+      connection.query(addSql,addSqlParams,function (err, result) {
+              if(err){
+               console.log('[INSERT ERROR] - ',err.message);
+               return;
+              }        
+       
+             console.log('--------------------------INSERT----------------------------');
+             console.log('INSERT ID:',result);        
+             console.log('-----------------------------------------------------------------\n\n');  
+      });
+       
+      connection.end();
+
+
+    // console.log("Name : " + name + ", Account : " + account + ", Password : " + password);
+
+
 });
 
 router.post('/register2', function(req, res, next){
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var date = req.body.date;
-    var phone = req.body.phone;
-    var credit = req.body.credit;
-    var gender = req.body.gender;
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let date = req.body.date;
+    let phone = req.body.phone;
+    let credit = req.body.credit;
+    let gender = req.body.gender;
     
     console.log("fname : " + fname + ", lname : " + lname + ", Gender : " + gender + ", date : " + date + ", phone : " + phone + ", credit : " + credit);
     
 });
 
 router.post('/register3', function(req, res, next){
-    var mailAddr = req.body.mail;
+    let mailAddr = req.body.mail;
     console.log("Mail : " + mailAddr);
 
     var transporter = nodemailer.createTransport({
