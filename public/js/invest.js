4bd968f5cfd371352713753e97be80746e3d963f
yuting6304 <s5333815@yahoo.com.tw>
2019-09-02 22:21:28 +0800

    Update final version

--- public/js/invest.js
+++ public/js/invest.js
@@ -33,17 +33,17 @@ window.onload = function(){
 
 function countdown(count_down_time){
     for(let i = 0; i < count_down_time.length; i++){
-        // if(count_down_time[i] == 0 && table.rows[i+1].cells[9].innerHTML == 1){
-        //     console.log(count_down_time[i]);
+        if(count_down_time[i] == 0 && table.rows[i+1].cells[9].innerHTML == 1){
+            console.log(count_down_time[i]);
 
-        //     console.log(table.rows[i].cells[9].innerHTML);
+            console.log(table.rows[i].cells[9].innerHTML);
 
-        //     var serverURL = "http://localhost:8088/invest/changeStatus?"+"user="+table.rows[i+1].cells[1].innerHTML+"&index="+table.rows[i+1].cells[10].innerHTML;
-        //     var xhr = new XMLHttpRequest();
-        //     xhr.open('POST',serverURL,true);
-        //     xhr.withCredentials=false;
-        //     xhr.send();
-        // }
+            var serverURL = "http://localhost:8088/invest/changeStatus?"+"user="+table.rows[i+1].cells[1].innerHTML+"&index="+table.rows[i+1].cells[10].innerHTML;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST',serverURL,true);
+            xhr.withCredentials=false;
+            xhr.send();
+        }
         count_down_time[i] = count_down_time[i]-1;
         if(count_down_time[i] >= 0){
             seconds = (count_down_val[i]%60);
